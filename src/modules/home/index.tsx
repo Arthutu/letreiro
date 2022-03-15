11d2114ac3757e1938cf7c52f8d3643fbd5c24f6
Arthutu <arthurrodolfo81@gmail.com>
2022-03-15 19:39:30 -0300

    Rename isGameWon to isGameFinished

--- src/modules/home/index.tsx
+++ src/modules/home/index.tsx
@@ -22,7 +22,7 @@ export const Home = (): JSX.Element => {
   const [currentWord, setCurrentWord] = useState<string>("");
   const [words, setWords] = useState<string[]>([]);
   const [lettersTryed, setLettersTryed] = useState<string[]>([]);
-  const [isGameWon, setIsGameWon] = useState<boolean>(false);
+  const [isGameFinished, setIsGameFinished] = useState<boolean>(false);
   const [isWrongWord, setIsWrongWord] = useState<boolean>(false);
   const [backSpacePressed, setBackSpacePressed] = useState<boolean>(false);
   const [isLoadFromLocalStorage, setIsLoadFromLocalStorage] =
@@ -31,7 +31,7 @@ export const Home = (): JSX.Element => {
   const dailyWord = getTodaysWord();
 
   const onLetterPress = (letter: string): void => {
-    if (!isGameWon && currentWord.length < MAX_LETTERS) {
+    if (!isGameFinished && currentWord.length < MAX_LETTERS) {
       setCurrentWord(currentWord.concat(letter));
       setLettersTryed([...lettersTryed, letter]);
     }
@@ -41,7 +41,7 @@ export const Home = (): JSX.Element => {
   };
 
   const onEnterPress = (): void => {
-    if (!isGameWon) {
+    if (!isGameFinished) {
       if (isLoadFromLocalStorage) {
         setIsLoadFromLocalStorage(false);
       }
@@ -55,10 +55,11 @@ export const Home = (): JSX.Element => {
         LocalStorageHelper.updateWords(newWords);
 
         if (currentWord === dailyWord) {
-          setIsGameWon(true);
+          setIsGameFinished(true);
           LocalStorageHelper.updateGameWin(newWords.length);
           alert("Você ganhou!");
         } else if (newWords.length >= MAX_WORDS) {
+          setIsGameFinished(true);
           LocalStorageHelper.updateGameLose();
           alert("Você perdeu!");
         }
@@ -69,7 +70,7 @@ export const Home = (): JSX.Element => {
   };
 
   const onBackspacePress = (): void => {
-    if (!isGameWon) {
+    if (!isGameFinished) {
       setBackSpacePressed(true);
       setIsWrongWord(false);
       setCurrentWord(currentWord.slice(0, -1));
@@ -101,10 +102,11 @@ export const Home = (): JSX.Element => {
       const tries = LocalStorageHelper.getWords();
       setIsLoadFromLocalStorage(true);
       setWords(tries);
-      setIsGameWon(LocalStorageHelper.getIsGameWon());
+      setIsGameFinished(LocalStorageHelper.getIsGameFinished());
     } else {
       LocalStorageHelper.updateLastWordIndex(getWordIndex().toString());
       LocalStorageHelper.initializeLocalStorage();
+      window.location.reload();
     }
   }, []);
 
@@ -149,10 +151,10 @@ export const Home = (): JSX.Element => {
               currentWord={currentWord}
               words={words}
               dailyWord={dailyWord}
-              isGameWon={isGameWon}
+              isGameFinished={isGameFinished}
               isWrongWord={isWrongWord}
               backSpacePressed={backSpacePressed}
-              isLoadFromLocalStorage={false}
+              isLoadFromLocalStorage={isLoadFromLocalStorage}
             />
           </Grid>
           <Grid item xs display="flex" alignItems="center">
@@ -165,56 +167,6 @@ export const Home = (): JSX.Element => {
               dailyWord={dailyWord}
             />
           </Grid>
-        </Grid>
-        <Grid
-          item
-          display="flex"
-          justifyContent="center"
-          marginBottom="1em"
-          marginTop="1em"
-        >
-          <Grow in={isWrongWord}>
-            <Alert
-              severity="error"
-              variant="filled"
-              sx={{ width: "20em" }}
-              action={
-                <IconButton
-                  aria-label="close"
-                  color="inherit"
-                  size="small"
-                  onClick={() => {
-                    setIsWrongWord(false);
-                  }}
-                >
-                  <CloseIcon fontSize="inherit" />
-                </IconButton>
-              }
-            >
-              A palavra não é válida
-            </Alert>
-          </Grow>
-        </Grid>
-        <Grid item xs>
-          <WordsGrid
-            currentWord={currentWord}
-            words={words}
-            dailyWord={dailyWord}
-            isGameWon={isGameWon}
-            isWrongWord={isWrongWord}
-            backSpacePressed={backSpacePressed}
-            isLoadFromLocalStorage={isLoadFromLocalStorage}
-          />
-        </Grid>
-        <Grid item xs display="flex" alignItems="center">
-          <Keyboard
-            onBackspacePress={onBackspacePress}
-            onLetterPress={onLetterPress}
-            onEnterPress={onEnterPress}
-            areLettersDisalabled={currentWord.length >= 5}
-            words={words}
-            dailyWord={dailyWord}
-          />
         </Grid>
       </Container>
     </Paper>
