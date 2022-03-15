26791cc092d1b03edc4f36d93084fd1feb1d9e93
Arthutu <arthurrodolfo81@gmail.com>
2022-03-15 20:58:28 -0300

    Added social medias informations on settings modal

--- src/modules/home/header/components/settings-modal/content.tsx
+++ src/modules/home/header/components/settings-modal/content.tsx
@@ -1,6 +1,15 @@
-import { Stack, Switch, Typography, useTheme } from "@mui/material";
-import React from "react";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import VideogameAssetIcon from "@mui/icons-material/VideogameAsset";
+import {
+  IconButton,
+  Link,
+  Stack,
+  Switch,
+  Typography,
+  useTheme,
+} from "@mui/material";
 import { ColorModeContext } from "common/contexts/color-theme-context";
+import React from "react";
 
 export const SettingsModalContent = (): JSX.Element => {
   const colorMode = React.useContext(ColorModeContext);
@@ -23,6 +32,37 @@ export const SettingsModalContent = (): JSX.Element => {
           onChange={colorMode.toggleColorBlindMode}
           color="success"
         />
+      </Stack>
+      <Stack direction="row" spacing={0.7}>
+        <Typography>Letereiro é baseado em</Typography>
+
+        <Link
+          color="#0b63e3"
+          href="https://www.nytimes.com/games/wordle/index.html"
+        >
+          Wordle
+        </Link>
+      </Stack>
+      <Stack direction="row" spacing={0.7}>
+        <Typography>Desenvolvido por</Typography>
+
+        <Link color="#0b63e3" href="https://twitter.com/arthurlrodolfo">
+          Arthur Rodolfo
+        </Link>
+
+        <Typography>e</Typography>
+
+        <Link color="#0b63e3" href="https://www.instagram.com/vitor_mrp/">
+          Vitor Pereira
+        </Link>
+      </Stack>
+      <Stack direction="row">
+        <IconButton href="https://github.com/Arthutu/letreiro">
+          <GitHubIcon></GitHubIcon>
+        </IconButton>
+        <IconButton href="https://www.backloggd.com/u/Arthutu/">
+          <VideogameAssetIcon></VideogameAssetIcon>
+        </IconButton>
       </Stack>
     </Stack>
   );
